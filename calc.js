ead4d63c7c9221e9c6ee57be036580f000858806
Ayush Tiwari <teltorob@gmail.com>
2021-02-14 18:57:25 +0530

    initial commit

--- calc.js
+++ calc.js
@@ -1,20 +1,104 @@
-var no0 = document.getElementById("0");
-var no1 = document.getElementById("1");
-var no2 = document.getElementById("2");
-var no3 = document.getElementById("3");
-var no4 = document.getElementById("4");
-var no5 = document.getElementById("5");
-var no6 = document.getElementById("6");
-var no7 = document.getElementById("7");
-var no8 = document.getElementById("8");
-var no9 = document.getElementById("9");
-var dot = document.getElementById(".");
 
-var function_add = document.getElementById("add");
-var function_add = document.getElementById("substract");
-var function_add = document.getElementById("multiply");
-var function_add = document.getElementById("divide");
-var function_add = document.getElementById("equals");
-var function_add = document.getElementById("percent");
-var function_add = document.getElementById("all-clear");
-var function_add = document.getElementById("invert");
+const display= document.getElementById("display");
+var display_string="";
+var display_number=0;
+var operation_type="";
+var first_number=0;
+var second_number=0;
+
+function press(value)
+{
+    
+    display_string+=value;
+    display.innerText=display_string;
+    display_number=parseFloat(display_string );
+}
+
+function binary_operation(type)
+{
+    first_number=display_number;
+    display_string="";
+    operation_type=type;
+}
+function equals()
+{
+    second_number=display_number;
+    switch(operation_type)
+    {
+        case "add":
+            {
+                display_number= parseFloat(add());
+                get_ready();
+               
+                break;
+            }
+        case "substract":
+            {
+                display_number=parseFloat(substract());
+                get_ready();
+                break;
+            }
+    
+        case "divide":
+            {
+                display_number=parseFloat(divide());
+                get_ready();
+                break;
+            }
+        case "multiply":
+            {
+                display_number=parseFloat(multiply());
+                get_ready();
+                break;
+            }
+
+                            
+    }
+}
+    
+function add()
+{
+     return(first_number+second_number).toFixed(8);
+}
+
+function substract()
+{
+    return (first_number-second_number).toFixed(8);
+}
+
+function multiply()
+{
+    return (first_number*second_number).toFixed(8);
+}
+
+function divide()
+{
+    return (first_number/second_number).toFixed(8);
+}
+
+function percent()
+{
+    display_number=(display_number/100);
+    get_ready();
+
+}
+
+function invert()
+{
+    display_number=(display_number*-1);
+    get_ready();
+}
+
+function all_clear()
+{
+    first_number,second_number=0;
+    display_number=0;
+    get_ready();
+}
+
+function get_ready()
+{
+    display_string=display_number.toString();
+    display.innerText=display_string;
+    display_string="";
+}
